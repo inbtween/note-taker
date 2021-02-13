0b0292d0d6c69f39e135ee8f723e840065887293
Michelle Smith <smith.michelle.a@gmail.com>
2021-02-13 16:11:22 -0500

    added (s) to api/notes in apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -16,7 +16,7 @@ module.exports = (app) => {
 
   app.get("/api/title", (req, res) => res.json(titleData));
 
-  app.get("/api/note", (req, res) => res.json(noteData));
+  app.get("/api/notes", (req, res) => res.json(noteData));
 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
@@ -26,18 +26,29 @@ module.exports = (app) => {
   // Then the server saves the data to the titleData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/note", (req, res) => {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
+  app.post("/api/title", (req, res) => {
+    // title the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" make a title
     // req.body is available since we're using the body parsing middleware
-    if (noteData.length < 5) {
+    if (titleData.length < 20) {
+      titleData.push(req.body);
+      res.json(true);
+    } else {
+      titleData.push(req.body);
+      res.json(false);
+    }
+  });
+  app.post("/api/notes", (req, res) => {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" make a note
+    // req.body is available since we're using the body parsing middleware
+    if (noteData.length < 20) {
       noteData.push(req.body);
       res.json(true);
+    } else {
+      noteData.push(req.body);
+      res.json(false);
     }
-    // } else {
-    //   noteData.push(req.body);
-    //   res.json(false);
-    // }
   });
 
   // I added this below code so you could clear out the table while working with the functionality.
