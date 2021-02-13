7a67cf1276c33caf2208640bd042a696301e8db7
Michelle Smith <smith.michelle.a@gmail.com>
2021-02-12 22:36:52 -0500

    added res.sendFile(path.join(__dirname, ./public/index.html));

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
 // ROUTES
 app.get("/", (req, res) => {
-  res.sendFile("./public/index.html");
+  res.sendFile(path.join(__dirname, "./public/index.html"));
   // res.send("The server is live");
 });
 app.listen(PORT, () => {
