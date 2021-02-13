3bbe29a724f9af229fd70fab54a064b1fc9f818d
Michelle Smith <smith.michelle.a@gmail.com>
2021-02-13 00:09:04 -0500

    added thge api and html routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,52 @@
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+
+const titleData = require("../data/note-title");
+const noteData = require("../data/note-text");
+
+// ROUTING
+
+module.exports = (app) => {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/title", (req, res) => res.json(titleData));
+
+  app.get("/api/note", (req, res) => res.json(noteData));
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the titleData array)
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/title", (req, res) => {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
+    if (titleData.length < 5) {
+      titleData.push(req.body);
+      res.json(true);
+    } else {
+      noteData.push(req.body);
+      res.json(false);
+    }
+  });
+
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  app.post("/api/clear", (req, res) => {
+    // Empty out the arrays of data
+    titleData.length = 0;
+    noteData.length = 0;
+
+    res.json({ ok: true });
+  });
+};
