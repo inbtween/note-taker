c97efb8978bce00db8b6ef51d49fc81e3a9a4de8
Michelle Smith <smith.michelle.a@gmail.com>
2021-02-12 23:27:22 -0500

    added /public to app.use

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 // a dynamic port
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+app.use(express.static("/public"));
 // ROUTES
 app.get("/", (req, res) => {
   // res.sendFile(path.join(__dirname, "index.html"));
